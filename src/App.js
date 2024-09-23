4e0c0abe481715b4041d78b386fc966b358c4f08
Divya Raj <draj56227@gmail.com>
2024-09-24 00:43:39 +0530

    hashroute

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import "./App.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Component/Home";
 import "antd/dist/antd.css";
 
